2d2c9ef6522c20442830d127cab4c4bd880e9e93
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-02-23 14:07:13 +0300

    Add the store to use with the application

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import casesReducer from './cases/casesSlice';
 import caseDetailsReducer from './cases/caseDetailsSlice';
+import authReducer from './auth/authSlice';
 
 const store = configureStore({
   reducer: {
     cases: casesReducer,
     caseDetails: caseDetailsReducer,
+    auth: authReducer,
   },
 });
 export default store;
