84840354cef46c029be6155538a112711c17d786
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-03-22 11:16:27 +0300

    Add search logic and operations

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,28 +1,29 @@
-import { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
   const [isCategoriesOpen, setIsCategoriesOpen] = useState(false);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+  const [keyword, setKeyword] = useState('');
+  const navigate = useNavigate();
 
   const toggleCategories = () => {
     setIsCategoriesOpen(!isCategoriesOpen);
   };
 
-  const handleSearch = async () => {
-    try {
-      const response = await axios.get(`/api/cases?q=${searchQuery}`);
-      setSearchResults(response.data);
-    } catch (error) {
-      console.error('Error fetching search results:', error);
-      setSearchResults([]);
+  const searchHandler = (e) => {
+    e.preventDefault()
+    
+    if(keyword.trim()){
+      navigate(`/search/${keyword}`)
+    }else{
+      navigate('/')
     }
-  };
+    
+}
 
   return (
     <>
-      <form onSubmit={handleSearch} className="max-w-lg mx-auto">
+      <form onSubmit={searchHandler} className="max-w-lg mx-auto">
         <div className="flex">
           {/* Dropdown for All categories */}
           <button
@@ -56,7 +57,7 @@ const Search = () => {
               id="search-dropdown"
               className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
               placeholder="Search..."
-              onChange={(e) => setSearchQuery(e.target.value)}
+              onChange={(e) => setKeyword(e.target.value)}
               required
             />
             <button
@@ -82,12 +83,6 @@ const Search = () => {
             </button>
           </div>
           <div>
-        {searchResults.map((result) => (
-          <div key={result.id}>
-            <h3>{result.title}</h3>
-            <p>{result.description}</p>
-          </div>
-        ))}
       </div>
         </div>
         <div
