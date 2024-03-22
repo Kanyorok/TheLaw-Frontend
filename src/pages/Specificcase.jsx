dcca7d956e53915f69b09947b3cdde23130678d6
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-03-22 06:23:29 +0300

    Adjust the design of the app

--- src/pages/Specificcase.jsx
+++ src/pages/Specificcase.jsx
@@ -1,34 +1,35 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { TableCell, TableRow } from '@mui/material';
 
 
 const Specificcase = ({ caseList }) => {
   const isOddRow = caseList.itemNumber % 2 === 1;
 
   return (
-    <tr className={isOddRow ? 'bg-neutral-100' : ''}>
-      <td className="w-1/6 font-bold whitespace-wrap border-b border-r px-6 py-4 dark:border-neutral-500">
+    <TableRow className={isOddRow ? 'bg-neutral-100' : ''}>
+      <TableCell className="w-1/6 font-bold whitespace-wrap border-b border-r px-6 py-4 dark:border-neutral-500">
         {caseList.itemNumber}
-      </td>
-      <td className="w-1/6 font-bold whitespace-wrap border-b border-r px-6 py-4 dark:border-neutral-500">
+      </TableCell>
+      <TableCell className="w-1/6 font-bold whitespace-wrap border-b border-r px-6 py-4 dark:border-neutral-500">
         <Link to={caseList ? `/cases/${caseList.case_id}` : '/'} className="card-link discover-link">
           {caseList.case_title}
         </Link>
-      </td>
-      <td className="w-3/6 whitespace-wrap border-b border-r px-6 py-4 dark:border-neutral-500">
+      </TableCell>
+      <TableCell className="w-3/6 whitespace-wrap border-b border-r px-6 py-4 dark:border-neutral-500">
         {caseList.description}
-      </td>
-    <td className="w-3/6 whitespace-wrap border-b border-r px-6 py-4 dark:border-neutral-500">
+      </TableCell>
+    <TableCell className="w-3/6 whitespace-wrap border-b border-r px-6 py-4 dark:border-neutral-500">
       <ul>
         {caseList.stakeholders && caseList.stakeholders.map((stakeholder, index) => (
           <li key={index}>{index+1}. {stakeholder}</li>
         ))}
       </ul>
-    </td>
+    </TableCell>
       <td className="w-3/6 whitespace-wrap border-b border-r px-6 py-4 dark:border-neutral-500">
         {caseList.status}
       </td>
-    </tr>
+    </TableRow>
   );
 };
 
