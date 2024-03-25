d7594f2dce4803bf398f9d37cc647962a9f23b03
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-03-25 18:05:02 +0300

    Fix the routes to the reservation component

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -175,7 +175,7 @@ const Header = () => {
                         data-te-nav-link-ref
                         to="/deletereservations"
                       >
-                        Delete Cases
+                        Delete Reservation
                       </NavLink>
                     </li>
                     <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
